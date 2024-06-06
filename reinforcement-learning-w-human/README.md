8f1dbf2d635ffaae5e8bfa2b4ae9d0023bcdcb8a
gary1030 <gary6658@ntu.im>
2024-06-06 22:21:44 +0800

    update readme

--- reinforcement-learning-w-human/README.md
+++ reinforcement-learning-w-human/README.md
@@ -1,5 +1,11 @@
 # RLHF
 
+## Environment
+
+1. GPU: RTX 3080 Ti
+2. GPU RAM: 12GB
+3. Pytorch 2.1.0
+
 ## Installation
 
 ```sh
@@ -10,4 +16,20 @@ pip install torch torchvision torchaudio
 pip install --no-deps trl peft accelerate bitsandbytes
 pip install tqdm packaging wandb
 pip install "unsloth[cu121-ampere] @ git+https://github.com/unslothai/unsloth.git"
+```
+
+or
+
+```sh
+conda create -y -n ai_hw6 python=3.10
+conda activate ai_hw6
+pip install -r requirements.txt
+```
+
+## Run
+
+```sh
+# bash run.sh <exp_name> <model_name> <wandb_token> <optimizer> <epoch> <beta>
+bash run.sh DPO unsloth/llama-3-8b-bnb-4bit <wandb_token> paged_adamw_32bit 1 0.1
+bash run.sh ORPO unsloth/llama-3-8b-bnb-4bit <wandb_token> paged_adamw_32bit 1 0.1
 ```
--- reinforcement-learning-w-human/run.sh
+++ reinforcement-learning-w-human/run.sh
@@ -1,31 +1,13 @@
 #!/bin/bash
 
-# python main.py \
-#     --exp_name "${1}" \
-#     --model_name "${2}" \
-#     --train \
-#     --wandb_token "${3}" \
-#     --num_epochs 1 \
-#     --max_prompt_length 512 \
-#     --train_batch_size 1 \
-#     --eval_batch_size 1 \
-
 python main.py \
-    --exp_name "DPO" \
-    --model_name "unsloth/llama-3-8b-bnb-4bit" \
+    --exp_name "${1}" \
+    --model_name "${2}" \
     --train \
-    --wandb_token "798d552535c4742bf29c5d005e9a6fe2f3addaac" \
-    --num_epochs 1 \
-    --optimizer "paged_adamw_32bit" \
+    --wandb_token "${3}" \
+    --num_epochs "${5}" \
+    --beta "${6}" \
+    --optimizer "${4}" \
     --train_batch_size 1 \
     --eval_batch_size 1 \
 
-python main.py \
-    --exp_name "ORPO" \
-    --model_name "unsloth/llama-3-8b-bnb-4bit" \
-    --train \
-    --wandb_token "798d552535c4742bf29c5d005e9a6fe2f3addaac" \
-    --num_epochs 1 \
-    --optimizer "paged_adamw_32bit" \
-    --train_batch_size 1 \
-    --eval_batch_size 1 \
